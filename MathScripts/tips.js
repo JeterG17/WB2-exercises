2d3cffe39feceaca6c22977b0c9eb4120ba05d73
JeterG17 <Jetguerrero02@gmail.com>
2023-10-17 15:13:47 -0400

    new javascript files

---
+++ MathScripts/tips.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var materialCost = 250.25;
+
+var tax = 22.75;
+
+var tips = 5.00;
+
+var totalDue = materialCost + tax + tips;
+
+console.log("the material is cost" + materialCost)
+
+console.log("tax is" + tax)
+
+console.log("the tip is" + tips);
+
+console.log("the total due is" + totalDue);
+
